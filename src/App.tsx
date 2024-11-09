5f5b0f85818fdf857c59960d5d6d81c33262e4e8
cristianchelu@gmail.com <cristianchelu@gmail.com>
2024-11-09 18:11:47 +0200

    remove vite boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import searchDevices from "./api/miot-spec/searchDevices";
+
+import "./App.css";
 
 function App() {
   async function debugSearchDevice() {
@@ -11,24 +10,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => debugSearchDevice()}>Test!</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>MIoT ➠ ESPHome</h1>
+      <button onClick={() => debugSearchDevice()}>Test!</button>
     </>
   );
 }
